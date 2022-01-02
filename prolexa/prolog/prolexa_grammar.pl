86d6c7a5e506576983c790980c11bab21b1143bf
Matt Clifford <mc15445@my.bristol.ac.uk>
2022-01-02 12:03:48 +0000

    tried negative to existential.

--- prolexa/prolog/prolexa_grammar.pl
+++ prolexa/prolog/prolexa_grammar.pl
@@ -167,7 +167,8 @@ question1(not(Q)) --> [does],proper_noun(_,X),neg_verb_phrase(_,X=>Q).
 %%% questions exists with skolem constants %%%
 question_e(Q) --> qword,question1_e(Q).
 question1_e((Q1,Q2)) --> [are,some],noun(p,sk=>Q1),property(p,sk=>Q2). % with skolem
-% question1_e((Q1,Q2)) --> [are,some],noun(p,_X=>Q1),property(p,X=>Q2).
+% negative version
+question1_e((Q1,not(Q2))) --> [are,some],noun(p,sk=>Q1), [not], property(p,sk=>Q2). % with skolem
 
 
 %%% commands %%%
